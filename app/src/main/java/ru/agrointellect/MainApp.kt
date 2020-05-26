c9e9858d3e72d7f6cc2d5f4b2c23552f3c1c458e
vlad <vladkalyuzhnyu@gmail.com>
2020-05-26 11:58:02 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/MainApp.kt
@@ -0,0 +1,26 @@
+package ru.agrointellect
+
+import android.app.Application
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+@Suppress("unused")
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath(null)
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
+}
