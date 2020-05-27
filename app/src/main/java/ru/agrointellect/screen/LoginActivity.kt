ddf2aa5336336439f7985e512c4fa0827395d583
vlad <vladkalyuzhnyu@gmail.com>
2020-05-27 14:15:23 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/LoginActivity.kt
+++ app/src/main/java/ru/agrointellect/screen/LoginActivity.kt
@@ -1,13 +1,19 @@
 package ru.agrointellect.screen
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 import ru.agrointellect.R
 import ru.agrointellect.screen.base.BaseActivity
+import ru.agrointellect.screen.main.MainActivity
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mb_login.setOnClickListener {
+            startActivity<MainActivity>()
+        }
     }
 }
