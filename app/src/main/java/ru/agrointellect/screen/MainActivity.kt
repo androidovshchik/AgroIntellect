5ac1c486a19a03fc45ba866b9cef82d68a89d87f
vlad <vladkalyuzhnyu@gmail.com>
2020-05-27 10:13:12 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/MainActivity.kt
+++ app/src/main/java/ru/agrointellect/screen/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned.SPAN_INCLUSIVE_INCLUSIVE
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.core.view.forEach
+import coil.api.load
+import coil.transform.CircleCropTransformation
 import io.github.inflationx.calligraphy3.CalligraphyTypefaceSpan
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_header.view.*
 import ru.agrointellect.R
 import ru.agrointellect.screen.base.BaseActivity
 
@@ -25,6 +29,24 @@ class MainActivity : BaseActivity() {
             it.title = SpannableString(it.title).apply {
                 setSpan(CalligraphyTypefaceSpan(font), 0, it.title.length, SPAN_INCLUSIVE_INCLUSIVE)
             }
+        }
+        val header = nv_main.getHeaderView(0)
+        header.fab_avatar.load(R.mipmap.ic_launcher_round) {
+            transformations(CircleCropTransformation())
+        }
+        header.ib_close.setOnClickListener {
+            dl_main.closeDrawers()
+        }
+        tv_exit.setOnClickListener {
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (dl_main.isDrawerOpen(GravityCompat.START)) {
+            dl_main.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed();
         }
     }
 }
