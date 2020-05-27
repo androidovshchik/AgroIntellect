11c68d1699a23014adce206a47e800c6f426dfc5
vlad <vladkalyuzhnyu@gmail.com>
2020-05-27 19:39:38 +0300

    changes

--- app/src/main/java/ru/agrointellect/MainApp.kt
+++ app/src/main/java/ru/agrointellect/MainApp.kt
@@ -7,24 +7,13 @@ import coil.ImageLoader
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
-import io.ktor.client.HttpClient
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.provider
-import org.kodein.di.generic.singleton
 import ru.agrointellect.local.localModule
+import ru.agrointellect.remote.remoteModule
 import timber.log.Timber
-
-class NetworkLogger : Logger {
-
-    override fun log(message: String) {
-        Timber.tag("API").d(message)
-    }
-}
 
 @Suppress("unused")
 class MainApp : Application(), KodeinAware {
@@ -37,16 +26,7 @@ class MainApp : Application(), KodeinAware {
 
         import(localModule)
 
-        bind<HttpClient>() with singleton {
-            HttpClient {
-                if (BuildConfig.DEBUG) {
-                    install(Logging) {
-                        logger = NetworkLogger()
-                        level = LogLevel.ALL
-                    }
-                }
-            }
-        }
+        import(remoteModule)
     }
 
     override fun onCreate() {
---
+++ app/src/main/java/ru/agrointellect/remote/RemoteModule.kt
@@ -0,0 +1,37 @@
+package ru.agrointellect.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+import ru.agrointellect.BuildConfig
+import timber.log.Timber
+
+class NetworkLogger : Logger {
+
+    override fun log(message: String) {
+        Timber.tag("API").d(message)
+    }
+}
+
+val remoteModule = Kodein.Module("remote") {
+
+    bind<HttpClient>() with singleton {
+        HttpClient {
+            if (BuildConfig.DEBUG) {
+                install(Logging) {
+                    logger = NetworkLogger()
+                    level = LogLevel.ALL
+                }
+            }
+            install(JsonFeature) {
+                serializer = GsonSerializer()
+            }
+        }
+    }
+}
