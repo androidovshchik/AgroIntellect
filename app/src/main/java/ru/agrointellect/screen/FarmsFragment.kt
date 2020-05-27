99a19722c0ad84562d29087765b04a04eaac2a98
vlad <vladkalyuzhnyu@gmail.com>
2020-05-27 07:44:01 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/screen/FarmsFragment.kt
@@ -0,0 +1,40 @@
+package ru.agrointellect.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.recyclical.datasource.dataSourceOf
+import com.afollestad.recyclical.setup
+import com.afollestad.recyclical.withItem
+import kotlinx.android.synthetic.main.fragment_farms.*
+import kotlinx.android.synthetic.main.item_farm.view.*
+import ru.agrointellect.R
+import ru.agrointellect.screen.base.BaseFragment
+
+class FarmHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val circle: ImageView = itemView.iv_circle
+    val name: TextView = itemView.tv_name
+}
+
+class FarmsFragment : BaseFragment() {
+
+    private val dataSource = dataSourceOf("")
+
+    override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_farms, root, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        rv_farms.setup {
+            withDataSource(dataSource)
+            withItem<String, FarmHolder>(R.layout.item_farm) {
+                onBind(::FarmHolder) { _, _ ->
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/ru/agrointellect/screen/MainActivity.kt
+++ app/src/main/java/ru/agrointellect/screen/MainActivity.kt
@@ -1,7 +1,12 @@
 package ru.agrointellect.screen
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned.SPAN_INCLUSIVE_INCLUSIVE
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.forEach
+import io.github.inflationx.calligraphy3.CalligraphyTypefaceSpan
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.agrointellect.R
 import ru.agrointellect.screen.base.BaseActivity
@@ -15,5 +20,11 @@ class MainActivity : BaseActivity() {
         val toggle = ActionBarDrawerToggle(this, dl_main, toolbar, 0, 0)
         dl_main.addDrawerListener(toggle)
         toggle.syncState()
+        val font = Typeface.createFromAsset(assets, "font/Ubuntu-Light.ttf")
+        nv_main.menu.forEach {
+            it.title = SpannableString(it.title).apply {
+                setSpan(CalligraphyTypefaceSpan(font), 0, it.title.length, SPAN_INCLUSIVE_INCLUSIVE)
+            }
+        }
     }
 }
