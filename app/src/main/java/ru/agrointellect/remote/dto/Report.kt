8b0108229a13e28eba4a62858d6572e3ad730fda
vlad <vladkalyuzhnyu@gmail.com>
2020-05-28 08:12:38 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/Report.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/Report.kt
@@ -9,6 +9,12 @@ class Report {
     @Expose
     lateinit var id: String
 
+    /**
+     * типы отчетов (report_type):
+     *   0 - только таблица
+     *   1 - только график
+     *   2 - таблица + график
+     */
     @SerializedName("report_type")
     @Expose
     lateinit var type: String
@@ -17,6 +23,11 @@ class Report {
     @Expose
     lateinit var name: String
 
+    /**
+     * флаг необходимости передачи диапазона дат для отчета (report_date_need)
+     *   0 - диапазон дат не требуется
+     *   1 - диапазон дат требуется
+     */
     @SerializedName("report_date_need")
     @Expose
     lateinit var dateNeed: String
