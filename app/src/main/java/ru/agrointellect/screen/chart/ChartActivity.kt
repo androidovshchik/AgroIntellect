7f8226c149f89935750382b20dfb6a746172efa2
vlad <vladkalyuzhnyu@gmail.com>
2020-05-29 14:47:22 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/screen/chart/ChartActivity.kt
@@ -0,0 +1,13 @@
+package ru.agrointellect.screen.chart
+
+import android.os.Bundle
+import ru.agrointellect.R
+import ru.agrointellect.screen.base.BaseActivity
+
+class ChartActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
