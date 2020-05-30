e5ed11931a877dac681e8c5fc65400e61376610d
vlad <vladkalyuzhnyu@gmail.com>
2020-05-30 07:50:12 +0300

    changes

--- app/src/main/java/ru/agrointellect/extension/Gson.kt
+++
@@ -1,8 +0,0 @@
-package ru.agrointellect.extension
-
-import com.google.gson.reflect.TypeToken
-import java.lang.reflect.Type
-
-fun <T> typeOfList(): Type {
-    return object : TypeToken<List<T>>() {}.type
-}
---
+++ app/src/main/java/ru/agrointellect/remote/dto/Table.kt
@@ -0,0 +1,30 @@
+package ru.agrointellect.remote.dto
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
+
+interface Table {
+
+    val columns: List<Column>
+}
+
+class Column(title: String, items: List<Row>) : ExpandableGroup<Row>(title, items)
+
+class Row(val key: String, val value: String) : Parcelable {
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {}
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Row> {
+
+        override fun createFromParcel(parcel: Parcel): Row? {
+            return null
+        }
+
+        override fun newArray(size: Int): Array<Row?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
