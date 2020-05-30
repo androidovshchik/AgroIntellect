ae69581c9919d7555bd3585df73ffaa2b9ab2296
vlad <vladkalyuzhnyu@gmail.com>
2020-05-30 10:10:49 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/TableFarmSummaryHistory.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/TableFarmSummaryHistory.kt
@@ -11,29 +11,15 @@ class TableFarmSummaryHistory : Table {
         get() = listOf(
             Column("№ лактации стада", items.map { Row(it.date, it.avgLactNum ?: "0") }),
             Column("День доения", items.map { Row(it.date, it.avgDaysInMilk ?: "0") }),
-            Column(
-                "День доения 1 лактации",
-                items.map { Row(it.date, it.avgDaysInMilk1Lact ?: "0") }),
-            Column(
-                "День доения 2 лактации",
-                items.map { Row(it.date, it.avgDaysInMilk2Lact ?: "0") }),
-            Column(
-                "День доения лактации > 2",
-                items.map { Row(it.date, it.avgDaysInMilkOver2Lact ?: "0") }),
+            Column("День доения 1 лактации", items.map { Row(it.date, it.avgDaysInMilk1Lact ?: "0") }),
+            Column("День доения 2 лактации", items.map { Row(it.date, it.avgDaysInMilk2Lact ?: "0") }),
+            Column("День доения лактации > 2", items.map { Row(it.date, it.avgDaysInMilkOver2Lact ?: "0") }),
             Column("Сервис-период", items.map { Row(it.date, it.avgOpenDays ?: "0") }),
-            Column(
-                "Сервис-период 1 лактации",
-                items.map { Row(it.date, it.avgOpenDays1Lact ?: "0") }),
-            Column(
-                "Сервис-период 2 лактации",
-                items.map { Row(it.date, it.avgOpenDays2Lact ?: "0") }),
-            Column(
-                "Сервис-период лактации > 2",
-                items.map { Row(it.date, it.avgOpenDaysOver2Lact ?: "0") }),
+            Column("Сервис-период 1 лактации", items.map { Row(it.date, it.avgOpenDays1Lact ?: "0") }),
+            Column("Сервис-период 2 лактации", items.map { Row(it.date, it.avgOpenDays2Lact ?: "0") }),
+            Column("Сервис-период лактации > 2", items.map { Row(it.date, it.avgOpenDaysOver2Lact ?: "0") }),
             Column("Кол-во дней в сухостое", items.map { Row(it.date, it.avgDryDays ?: "0") }),
-            Column(
-                "День стельности коров",
-                items.map { Row(it.date, it.avgPregnantDaysCows ?: "0") })
+            Column("День стельности коров", items.map { Row(it.date, it.avgPregnantDaysCows ?: "0") })
         )
 
     @SerializedName("rpt_farm_summary_history")
