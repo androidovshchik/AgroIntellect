b631a7110056ba2d70c4f8a9c2fc0ec68df8ff6e
vlad <vladkalyuzhnyu@gmail.com>
2020-05-30 08:36:07 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/TableHerdAlignmentNow.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/TableHerdAlignmentNow.kt
@@ -6,8 +6,16 @@ import com.google.gson.annotations.SerializedName
 class TableHerdAlignmentNow : Table {
 
     override val columns: List<Column>
-        get() {
-
+        get() = items.map {
+            Column(
+                "${it.groupNumber}. ${it.groupDescription}", listOf(
+                    Row("К-во животных", it.countOfAnimalsInGroup),
+                    Row("Надой сегодня", it.averageMilkToday),
+                    Row("Надой вчера", it.averageMilkYesterday),
+                    Row("День доения", it.averageDaysInMilk),
+                    Row("Ожидаемый надой", it.expectedAverageMilk)
+                )
+            )
         }
 
     @SerializedName("rpt_herd_alignment_now")
