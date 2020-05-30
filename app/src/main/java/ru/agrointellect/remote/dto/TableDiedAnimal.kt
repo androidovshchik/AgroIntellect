e91162afec594673481953ff5876dd89cd73d26d
vlad <vladkalyuzhnyu@gmail.com>
2020-05-30 13:00:08 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/remote/dto/TableDiedAnimal.kt
@@ -0,0 +1,22 @@
+package ru.agrointellect.remote.dto
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import ru.agrointellect.screen.report.Column
+import ru.agrointellect.screen.report.Row
+import ru.agrointellect.screen.report.Table
+
+class TableDiedAnimal : Table {
+
+    override val columns: List<Column>
+        get() = listOf(
+            Column("Падеж коров всего", items.map { Row(it.date, it.evtDeadCowsTotal ?: "0") }),
+            Column("Падеж нетелей", items.map { Row(it.date, it.evtDeadPheifers ?: "0") }),
+            Column("Падеж телок всего", items.map { Row(it.date, it.evtDeadHeifersTotal ?: "0") }),
+            Column("Падеж быков", items.map { Row(it.date, it.evtDeadBulls ?: "0") })
+        )
+
+    @SerializedName("rpt_died_animals")
+    @Expose
+    lateinit var items: List<RptDiedAnimal>
+}
