5dcd4ad8ba26cb78a9db0dcd213821cc1ee07d4f
vlad <vladkalyuzhnyu@gmail.com>
2020-05-30 15:26:26 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/remote/dto/TableMilkEventsKpi.kt
@@ -0,0 +1,34 @@
+package ru.agrointellect.remote.dto
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Надой, события, кормление
+ */
+@Suppress("SpellCheckingInspection")
+class TableMilkEventsKpi : Table {
+
+    override val columns: List<Column>
+        get() = listOf(
+            Column("Валовой надой", mapByField("mlkMilkSumYield")),
+            Column("Надой 1 ф/к", mapByField("mlkMilkPerCow")),
+            Column("Надой 1 д/к", mapByField("mlkMilkPerLactCow")),
+            Column("Осеменений коров", mapByField("evtInsemCows")),
+            Column("Выявлено стельных коров", mapByField("evtGotPregCows")),
+            Column("Запущено коров", mapByField("evtDryCows")),
+            Column("Переводов коров", mapByField("evtMoveCows")),
+            Column("Вакцинация коров", mapByField("evtVaccCows")),
+            Column("Расчистка копыт коров", mapByField("evtFootrimCows")),
+            Column("KPI кормления", mapByField("feedKpi"))
+        )
+
+    private fun mapByField(name: String): List<Row> {
+        val field = RptMilkEventsKpi::class.java.getField(name)
+        return items.map { Row(it.date, field.get(it)?.toString() ?: "0") }
+    }
+
+    @SerializedName("rpt_milk_events_kpi")
+    @Expose
+    lateinit var items: List<RptMilkEventsKpi>
+}
