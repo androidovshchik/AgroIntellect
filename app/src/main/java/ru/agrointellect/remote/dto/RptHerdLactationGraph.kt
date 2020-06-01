7749ddeec5ca0e6ccd84b834b350695d4d36e710
vlad <vladkalyuzhnyu@gmail.com>
2020-06-01 18:49:05 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/RptHerdLactationGraph.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/RptHerdLactationGraph.kt
@@ -11,25 +11,77 @@ class RptHerdLactationGraph {
 
     @SerializedName("average_lactation_daily_milk")
     @Expose
-    lateinit var averageLactationDailyMilk: String
+    var averageLactationDailyMilk: String? = null
 
     @SerializedName("lactation_1_daily_milk")
     @Expose
-    lateinit var lactation1DailyMilk: String
+    var lactation1DailyMilk: String? = null
 
     @SerializedName("lactation_2_daily_milk")
     @Expose
-    lateinit var lactation2DailyMilk: String
+    var lactation2DailyMilk: String? = null
 
     @SerializedName("lactation_over_2_daily_milk")
     @Expose
-    lateinit var lactationOver2DailyMilk: String
+    var lactationOver2DailyMilk: String? = null
+
+    @SerializedName("sample_lactation_5000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation5000: String? = null
+
+    @SerializedName("sample_lactation_5500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation5500: String? = null
+
+    @SerializedName("sample_lactation_6000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation6000: String? = null
+
+    @SerializedName("sample_lactation_6500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation6500: String? = null
 
     @SerializedName("sample_lactation_7000_per_305_milking_days_daily_milk")
     @Expose
-    lateinit var sampleLactation7000Per305MilkingDaysDailyMilk: String
+    var sampleLactation7000: String? = null
 
     @SerializedName("sample_lactation_7500_per_305_milking_days_daily_milk")
     @Expose
-    lateinit var sampleLactation7500Per305MilkingDaysDailyMilk: String
+    var sampleLactation7500: String? = null
+
+    @SerializedName("sample_lactation_8000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation8000: String? = null
+
+    @SerializedName("sample_lactation_8500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation8500: String? = null
+
+    @SerializedName("sample_lactation_9000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation9000: String? = null
+
+    @SerializedName("sample_lactation_9500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation9500: String? = null
+
+    @SerializedName("sample_lactation_10000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation10000: String? = null
+
+    @SerializedName("sample_lactation_10500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation10500: String? = null
+
+    @SerializedName("sample_lactation_11000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation11000: String? = null
+
+    @SerializedName("sample_lactation_11500_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation11500: String? = null
+
+    @SerializedName("sample_lactation_12000_per_305_milking_days_daily_milk")
+    @Expose
+    var sampleLactation12000: String? = null
 }
