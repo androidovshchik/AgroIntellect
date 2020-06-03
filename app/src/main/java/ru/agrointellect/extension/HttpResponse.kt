93c63cb3617e10f45236b63e94085318b9458e2d
vlad <vladkalyuzhnyu@gmail.com>
2020-06-03 17:36:20 +0300

    changes

--- app/src/main/java/ru/agrointellect/extension/HttpResponse.kt
+++ app/src/main/java/ru/agrointellect/extension/HttpResponse.kt
@@ -14,8 +14,38 @@ suspend inline fun <reified T> HttpResponse.readObject(
     depth: Int,
     vararg keys: String
 ): T {
+    return readJson(depth, *keys) {
+        val cls = when (keys.lastOrNull()) {
+            "rpt_herd_distribution" -> RptsHerdDistribution::class.java
+            "rpt_herd_alignment_now" -> RptsHerdAlignmentNow::class.java
+            "rpt_herd_alignment_history" -> RptsHerdAlignmentHistory::class.java
+            "rpt_herd_lactation_graph" -> RptsHerdLactationGraph::class.java
+            "rpt_milk_events_kpi" -> RptsMilkEventsKpi::class.java
+            "rpt_fresh_disease" -> RptsFreshDisease::class.java
+            "rpt_farm_summary_history" -> RptsFarmSummaryHistory::class.java
+            "rpt_herd_forecast" -> RptsHerdForecast::class.java
+            "rpt_sold_animals" -> RptsSoldAnimal::class.java
+            "rpt_died_animals" -> RptsDiedAnimal::class.java
+            "rpt_last_updates" -> RptsLastUpdate::class.java
+            else -> T::class.java
+        }
+        gson.fromJson(it, cls) as T
+    }
+}
+
+suspend inline fun <reified T> HttpResponse.readArray(gson: Gson, vararg keys: String): List<T> {
+    return readJson(keys.size, *keys) {
+        gson.fromJson(it, TypeToken.getParameterized(List::class.java, T::class.java).type)
+    }
+}
+
+suspend inline fun <T> HttpResponse.readJson(
+    depth: Int,
+    vararg keys: String,
+    block: (String) -> T
+): T {
     var text = readText()
-    return try {
+    try {
         if (keys.isNotEmpty()) {
             var json: Any = JSONObject(text)
             (0 until min(depth, keys.size)).forEach {
@@ -23,39 +53,7 @@ suspend inline fun <reified T> HttpResponse.readObject(
             }
             text = json.toString()
         }
-        gson.fromJson(
-            text, when (keys.lastOrNull()) {
-                "rpt_herd_distribution" -> RptsHerdDistribution::class.java
-                "rpt_herd_alignment_now" -> RptsHerdAlignmentNow::class.java
-                "rpt_herd_alignment_history" -> RptsHerdAlignmentHistory::class.java
-                "rpt_herd_lactation_graph" -> RptsHerdLactationGraph::class.java
-                "rpt_milk_events_kpi" -> RptsMilkEventsKpi::class.java
-                "rpt_fresh_disease" -> RptsFreshDisease::class.java
-                "rpt_farm_summary_history" -> RptsFarmSummaryHistory::class.java
-                "rpt_herd_forecast" -> RptsHerdForecast::class.java
-                "rpt_sold_animals" -> RptsSoldAnimal::class.java
-                "rpt_died_animals" -> RptsDiedAnimal::class.java
-                "rpt_last_updates" -> RptsLastUpdate::class.java
-                else -> T::class.java
-            }
-        ) as T
-    } catch (e: Throwable) {
-        Timber.e(e)
-        throw Throwable("Ошибка: $text")
-    }
-}
-
-suspend inline fun <reified T> HttpResponse.readArray(gson: Gson, vararg keys: String): List<T> {
-    var text = readText()
-    return try {
-        if (keys.isNotEmpty()) {
-            var json: Any = JSONObject(text)
-            keys.forEach {
-                json = (json as JSONObject).get(it)
-            }
-            text = json.toString()
-        }
-        gson.fromJson(text, TypeToken.getParameterized(List::class.java, T::class.java).type)
+        return block(text)
     } catch (e: Throwable) {
         Timber.e(e)
         throw Throwable("Ошибка: $text")
