507424ec90be12b1ccc534305c1fdb61afe60338
vlad <vladkalyuzhnyu@gmail.com>
2020-06-03 18:33:26 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/DatesDialog.kt
+++ app/src/main/java/ru/agrointellect/screen/DatesDialog.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.dialog_dates.*
-import kotlinx.android.synthetic.main.layout_calendar_container.*
-import ru.agrointellect.R
+import com.appeaser.sublimepickerlibrary.SublimePicker
+import com.appeaser.sublimepickerlibrary.datepicker.SelectedDate
+import com.appeaser.sublimepickerlibrary.helpers.SublimeListenerAdapter
+import com.appeaser.sublimepickerlibrary.helpers.SublimeOptions
+import com.appeaser.sublimepickerlibrary.recurrencepicker.SublimeRecurrencePicker
+import org.jetbrains.anko.wrapContent
 import ru.agrointellect.screen.base.BaseDialogFragment
 import ru.agrointellect.screen.report.ReportModel
 import java.text.SimpleDateFormat
@@ -17,37 +20,45 @@ class DatesDialog : BaseDialogFragment() {
 
     private lateinit var reportModel: ReportModel
 
+    private val listener = object : SublimeListenerAdapter() {
+
+        override fun onDateTimeRecurrenceSet(
+            sublimeMaterialPicker: SublimePicker?,
+            selectedDate: SelectedDate,
+            hourOfDay: Int,
+            minute: Int,
+            recurrenceOption: SublimeRecurrencePicker.RecurrenceOption?,
+            recurrenceRule: String?
+        ) {
+            reportModel.apply {
+                dateFrom = selectedDate.startDate?.let {
+                    formatter.format(it.time)
+                }
+                dateTo = selectedDate.endDate?.let {
+                    formatter.format(it.time)
+                }
+            }
+            dismiss()
+        }
+
+        override fun onCancelled() {
+            dismiss()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         reportModel = ViewModelProvider(requireActivity()).get(ReportModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
-        return inflater.inflate(R.layout.dialog_dates, root, false)
-    }
-
-    @Suppress("UNNECESSARY_SAFE_CALL")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        toolbar.apply {
-            setNavigationIcon(R.drawable.ic_close_24dp)
-            setNavigationOnClickListener {
-                dismiss()
-            }
-            inflateMenu(R.menu.menu_dates)
-            setOnMenuItemClickListener { item ->
-                if (item.itemId == R.id.action_done) {
-                    reportModel.apply {
-                        dateFrom = cv_dates.startDate?.let {
-                            formatter.format(it.time)
-                        }
-                        dateTo = cv_dates.endDate?.let {
-                            formatter.format(it.time)
-                        }
-                    }
-                    dismiss()
-                }
-                return@setOnMenuItemClickListener true
-            }
+        return SublimePicker(requireContext()).apply {
+            layoutParams = ViewGroup.LayoutParams(wrapContent, wrapContent)
+            initializePicker(SublimeOptions().apply {
+                pickerToShow = SublimeOptions.Picker.DATE_PICKER
+                setDisplayOptions(SublimeOptions.ACTIVATE_DATE_PICKER)
+                setCanPickDateRange(true)
+            }, listener)
         }
     }
 
