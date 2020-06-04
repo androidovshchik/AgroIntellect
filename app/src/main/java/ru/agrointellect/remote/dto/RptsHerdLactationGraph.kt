02ea632bbf9299147215f823300ffd0e494faa20
vlad <vladkalyuzhnyu@gmail.com>
2020-06-04 12:22:11 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/RptsHerdLactationGraph.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/RptsHerdLactationGraph.kt
@@ -3,7 +3,7 @@ package ru.agrointellect.remote.dto
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import de.siegmar.fastcsv.reader.CsvReader
-import timber.log.Timber
+import java.io.StringReader
 
 /**
  * График лактации поголовья
@@ -32,38 +32,24 @@ class RptsHerdLactationGraph : Table {
                 columns.add("Лактация > 2")
                 text.appendln(it)
             }
+            val first = "_lactation_"
             items[0].sampleLactations.forEach {
-                val first = "lactation_"
                 val start = it.key.indexOf(first)
                 val end = it.key.indexOf("_per_")
                 if (start in 0 until end) {
-                    columns.add(
-                        "Образец ${it.key.substring(
-                            start + first.length,
-                            end
-                        )} за 305 дней"
-                    )
+                    val num = it.key.substring(start + first.length, end)
+                    columns.add("Образец $num за 305 дней")
                     text.appendln(it.value)
                 }
             }
-            Timber.e(columns.toString())
             val csvReader = CsvReader()
-            /*val source = """
-                ${items[0].lactationDays}
-                ${items[0].averageLactationDailyMilk}
-                ${items[0].lactation1DailyMilk}
-                ${items[0].lactation2DailyMilk}
-                ${items[0].lactationOver2DailyMilk}
-                ${items[0].sampleLactation7500Per305MilkingDaysDailyMilk}
-            """.trimIndent()
-            val csv = csvReader.read(StringReader(source))
+            val csv = csvReader.read(StringReader(text.toString().trim()))
             val days = csv.getRow(0)
-            return columns.mapIndexedNotNull { i, column ->
+            return columns.mapIndexed { i, column ->
                 Column(column, csv.getRow(i + 1).fields.mapIndexed { j, value ->
                     Row("${days.getField(j)} день доения", value)
                 })
-            }*/
-            return emptyList()
+            }
         }
 
     @SerializedName("rpt_herd_lactation_graph")
