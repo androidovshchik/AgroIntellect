135c8d5edf96aa3bd4a08b43880f2b669ae50316
vlad <vladkalyuzhnyu@gmail.com>
2020-06-04 13:51:18 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/Farm.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/Farm.kt
@@ -1,9 +1,11 @@
 package ru.agrointellect.remote.dto
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Keep
 class Farm : Serializable {
 
     @SerializedName("farm_id")
