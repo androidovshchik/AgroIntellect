b058e261a23ef5a34fbb030828c9882722cbbeba
vlad <vladkalyuzhnyu@gmail.com>
2020-06-06 10:06:25 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/remote/dto/Chart.kt
@@ -0,0 +1,13 @@
+package ru.agrointellect.remote.dto
+
+interface ChartLine {
+
+}
+
+interface ChartStackedBar {
+
+}
+
+interface ChartGroupedBar {
+
+}
---
+++ app/src/main/java/ru/agrointellect/screen/chart/StackedBarFragment.kt
@@ -0,0 +1,19 @@
+package ru.agrointellect.screen.chart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.agrointellect.R
+import ru.agrointellect.screen.base.BaseFragment
+
+class StackedBarFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_chart, root, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
