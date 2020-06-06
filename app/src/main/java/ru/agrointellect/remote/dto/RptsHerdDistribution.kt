1d712704a7f2d572c412c94e9be8941c577b506a
vlad <vladkalyuzhnyu@gmail.com>
2020-06-06 12:24:19 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/RptsHerdDistribution.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/RptsHerdDistribution.kt
@@ -1,19 +1,29 @@
 package ru.agrointellect.remote.dto
 
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-/**
- * Поголовье: фуражное, дойное, стельное
- */
 @Suppress("SpellCheckingInspection")
-class RptsHerdDistribution : Table {
+class RptsHerdDistribution : Table, ChartBar {
 
     override val columns: List<Column>
         get() = listOf(
             Column("Фуражных коров", items.map { Row(it.date, it.hrdCowsAll) }),
             Column("Дойных коров всего", items.map { Row(it.date, it.hrdCowsLactAll) }),
             Column("Стельных коров", items.map { Row(it.date, it.hrdCowsPregAll) })
+        )
+
+    override val data: BarData
+        get() = BarData(
+            BarDataSet(items.map {
+                val val1 = it.hrdCowsPregAll?.toFloatOrNull() ?: 0f
+                val val2 = it.hrdCowsLactAll?.toFloatOrNull()?.minus(val1) ?: 0f
+                val val3 = it.hrdCowsAll?.toFloatOrNull()?.minus(val1 + val2) ?: 0f
+                BarEntry(parseDate(it.date), floatArrayOf(val1, val2, val3))
+            }, null)
         )
 
     @SerializedName("rpt_herd_distribution")
