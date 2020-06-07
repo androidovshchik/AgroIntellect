606e9a323a6fa9ccb26883cc817f0e7ad8e35500
vlad <vladkalyuzhnyu@gmail.com>
2020-06-07 10:46:51 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/ChrtsFarmSummaryHistory.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/ChrtsFarmSummaryHistory.kt
@@ -4,6 +4,7 @@ import com.github.mikephil.charting.data.*
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import ru.agrointellect.extension.asFloat
+import kotlin.math.min
 
 /**
  * График: Валовой надой + KPI
@@ -12,20 +13,17 @@ import ru.agrointellect.extension.asFloat
 open class ChrtsFarmSummaryHistory1 : ChartBase {
 
     override val legends: Collection<String>
-        get() = listOf(
-            "Валовой надой",
-            "KPI кормления"
-        )
+        get() = listOf("Валовой надой", "KPI кормления")
 
     override val data: ChartData<*>
         get() = LineData(
             listOf(
-                LineDataSet(mapByField("mlkMilkSumYield"), null),
-                LineDataSet(mapByField("feedKpi"), null)
+                LineDataSet(entryByField("mlkMilkSumYield"), null),
+                LineDataSet(entryByField("feedKpi"), null)
             )
         )
 
-    protected fun mapByField(name: String): List<Entry> {
+    protected fun entryByField(name: String): List<Entry> {
         val field = RptFarmSummaryHistory::class.java.getField(name)
         return items.map { Entry(parseDate(it.date), field.get(it).asFloat) }
     }
@@ -36,25 +34,36 @@ open class ChrtsFarmSummaryHistory1 : ChartBase {
 }
 
 /**
- * Грфик: Поголовье: фуражное, дойное, стельное
+ * График: Поголовье: фуражное, дойное, стельное
  */
 @Suppress("SpellCheckingInspection")
 class ChrtsFarmSummaryHistory2 : ChrtsFarmSummaryHistory1() {
 
     override val legends: Collection<String>
-        get() = listOf(
-            "Фуражных коров",
-            "Дойных коров всего",
-            "Стельных коров"
-        )
+        get() = listOf("Фуражных коров", "Дойных коров всего", "Стельных коров")
 
     override val data: ChartData<*>
         get() = BarData(
             BarDataSet(items.map {
-                val val1 = it.hrdCowsPregAll?.toFloatOrNull() ?: 0f
-                val val2 = it.hrdCowsLactAll?.toFloatOrNull()?.minus(val1) ?: 0f
-                val val3 = it.hrdCowsAll?.toFloatOrNull()?.minus(val1 + val2) ?: 0f
+                val val1 = it.hrdCowsPregAll.asFloat
+                val val2 = min(0f, it.hrdCowsLactAll.asFloat - val1)
+                val val3 = min(0f, it.hrdCowsAll.asFloat - val2 - val1)
                 BarEntry(parseDate(it.date), floatArrayOf(val1, val2, val3))
             }, null)
+        )
+}
+
+/**
+ * График: Кетозы
+ */
+@Suppress("SpellCheckingInspection")
+class ChrtsFarmSummaryHistory3 : ChrtsFarmSummaryHistory1() {
+
+    override val legends: Collection<String>
+        get() = listOf("Кетозов всего")
+
+    override val data: ChartData<*>
+        get() = BarData(
+            BarDataSet(items.map { BarEntry(parseDate(it.date), it.evtKetosTotal.asFloat) }, null)
         )
 }
