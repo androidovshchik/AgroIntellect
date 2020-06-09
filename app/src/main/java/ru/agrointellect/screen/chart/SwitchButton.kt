fe59f317639443aa2153d3280b82a8d7d1d68624
vlad <vladkalyuzhnyu@gmail.com>
2020-06-09 07:14:35 +0300

    changes

---
+++ app/src/main/java/ru/agrointellect/screen/chart/SwitchButton.kt
@@ -0,0 +1,25 @@
+package ru.agrointellect.screen.chart
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.switchmaterial.SwitchMaterial
+import ru.agrointellect.R
+
+class SwitchButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.switchStyle
+) : SwitchMaterial(context, attrs, defStyleAttr) {
+
+    var programmatically = false
+
+    override fun setChecked(checked: Boolean) {
+        programmatically = false
+        super.setChecked(checked)
+    }
+
+    fun setCheckedProgrammatically(checked: Boolean) {
+        programmatically = true
+        super.setChecked(checked)
+    }
+}
--- app/src/main/java/ru/agrointellect/screen/main/RadioButton.kt
+++ app/src/main/java/ru/agrointellect/screen/main/RadioButton.kt
@@ -3,11 +3,12 @@ package ru.agrointellect.screen.main
 import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.radiobutton.MaterialRadioButton
+import ru.agrointellect.R
 
 class RadioButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = android.R.attr.radioButtonStyle
+    defStyleAttr: Int = R.attr.radioButtonStyle
 ) : MaterialRadioButton(context, attrs, defStyleAttr) {
 
     override fun toggle() {}
