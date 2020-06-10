3eb354fdad74bd68548a566f8160fdcf37a1046f
vlad <vladkalyuzhnyu@gmail.com>
2020-06-10 09:57:19 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/report/ReportModel.kt
+++ app/src/main/java/ru/agrointellect/screen/report/ReportModel.kt
@@ -1,5 +1,26 @@
 package ru.agrointellect.screen.report
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.agrointellect.remote.dto.Farm
 import ru.agrointellect.remote.dto.RptDesc
+import java.util.*
 
-class ReportModel : DescModel<RptDesc>()
+open class ReportModel : ViewModel() {
+
+    lateinit var farm: Farm
+
+    private var rptDesc: RptDesc? = null
+
+    open fun setDesc(desc: RptDesc) {
+        rptDesc = desc
+    }
+
+    open fun getDesc() = rptDesc!!
+
+    var dateFrom: Date? = null
+
+    var dateTo: Date? = null
+
+    val datesChanged = MutableLiveData<Boolean>()
+}
