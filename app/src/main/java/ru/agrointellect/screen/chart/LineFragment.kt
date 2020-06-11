0eb2002065aaf3f0fa870294f2468560a31efcd8
vlad <vladkalyuzhnyu@gmail.com>
2020-06-11 13:29:13 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/chart/LineFragment.kt
+++ app/src/main/java/ru/agrointellect/screen/chart/LineFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import org.jetbrains.anko.matchParent
 import ru.agrointellect.remote.dto.GraphData
+import kotlin.math.max
+import kotlin.math.min
 
 class LineFragment : GraphFragment() {
 
@@ -20,14 +23,26 @@ class LineFragment : GraphFragment() {
     }
 
     override fun setData(data: GraphData) {
+        val lines = reportModel.getDesc().rightAxisLines
+        chart.apply {
+            axisRight.isEnabled = lines.isNotEmpty()
+        }
         (data as LineData).apply {
             dataSets.forEachIndexed { i, dataSet ->
-                val graphColor = getGraphColor(i)
                 (dataSet as LineDataSet).apply {
                     mode = reportModel.getDesc().lineMode
                     setDrawCircles(false)
                     lineWidth = 1.5f
-                    color = graphColor
+                    color = pickColor(i)
+                    if (i in lines) {
+                        axisDependency = YAxis.AxisDependency.RIGHT
+                        chart.axisRight.apply {
+                            axisMinimum = min(axisMinimum, yMin - 1)
+                            axisMaximum = max(axisMaximum, yMax + 1)
+                        }
+                    } else {
+                        axisDependency = YAxis.AxisDependency.LEFT
+                    }
                 }
             }
         }
