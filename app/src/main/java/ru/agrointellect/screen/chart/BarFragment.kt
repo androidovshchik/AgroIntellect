6c1cb0853cd42dfad83cc3a3ee533e8283a6d605
vlad <vladkalyuzhnyu@gmail.com>
2020-06-14 07:40:51 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/chart/BarFragment.kt
+++ app/src/main/java/ru/agrointellect/screen/chart/BarFragment.kt
@@ -15,19 +15,18 @@ class BarFragment : GraphFragment() {
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
         chart = BackBarChart(requireContext()).apply {
             layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
-            //setFitBars(true)
+            setFitBars(true)
             when {
                 reportModel.getDesc().isStackedBarChart -> {
                     xAxis.apply {
                         // 3/4 + 1/8
-                        spaceMin = 86400f * 7 / 8
-                        spaceMax = 86400f * 7 / 8
+                        spaceMin = DAY * 7 / 8
+                        spaceMax = DAY * 7 / 8
                     }
                 }
                 reportModel.getDesc().isGroupedBarChart -> {
-                    setScaleEnabled(false)
+                    //setScaleEnabled(false)
                     xAxis.setCenterAxisLabels(true)
-                    //xAxis.setLabelCount(7, true)
                 }
             }
         }
@@ -36,39 +35,43 @@ class BarFragment : GraphFragment() {
 
     /**
      * https://weeklycoding.com/mpandroidchart-documentation/setting-data
-     * 86400 = (w + s) * n + S <=> x * n + 0.75 * x
-     * 86400 = x * (n + 0.75) => x = 86400 / (n + 0.75)
-     * w = s = S / 1.5 = 86400 / (n + 0.75) / 2
+     * 86400 = (w + s) * n + S <=> 2x * n + 2x
+     * 86400 = 2x * (n + 1) => x = 86400 / (n + 1) / 2
      */
     override fun setData(data: GraphData) {
         (data as BarData).apply {
+            val count = getDataSetByIndex(0)?.entryCount ?: 0
             dataSets.forEachIndexed { i, dataSet ->
                 (dataSet as BarDataSet).apply {
-                    if (reportModel.getDesc().isStackedBarChart) {
-                        barWidth = 86400f / 4
-                        if (entryCount > 0) {
-                            colors = pickColors(getEntryForIndex(0).yVals.size)
+                    when {
+                        reportModel.getDesc().isStackedBarChart -> {
+                            barWidth = DAY / 4
+                            if (entryCount > 0) {
+                                colors = pickColors(getEntryForIndex(0).yVals.size)
+                            }
                         }
-                    } else {
-                        barWidth = 86400f / (dataSetCount + 0.75f) / 2
-                        color = pickColor(i)
+                        reportModel.getDesc().isGroupedBarChart -> {
+                            barWidth = DAY / (count + 1) / 2
+                            color = pickColor(i)
+                        }
                     }
                 }
             }
             if (reportModel.getDesc().isGroupedBarChart) {
-                if (dataSetCount > 1) {
-                    groupBars(xMin, barWidth * 3 / 2, barWidth)
+                if (count > 1) {
+                    groupBars(xMin, barWidth * 2, barWidth)
                 }
+                //chart.xAxis.axisMinimum = xMin + barWidth
+                Timber.e("xmin $xMin barWidth $barWidth")
+                Timber.e("= ${barWidth * 2 * (count + 1)}")
             }
-            chart.getXAxis().setAxisMinimum(xMin + barWidth * 3 / 2)
         }
         (chart as BackBarChart).drawBackground = reportModel.getDesc().isGroupedBarChart
         super.setData(data)
         chart.apply {
+            setVisibleXRangeMaximum(WEEK)
             //getXAxis().setAxisMinimum(data.xMin);
             //getXAxis().setAxisMaximum(data.xMin + data.dataSetCount * 86400f);
-            setVisibleXRangeMaximum(WEEK)
-            Timber.e("x ${chart.xAxis.position}")
         }
     }
 
