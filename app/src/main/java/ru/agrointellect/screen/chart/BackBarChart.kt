5609d2b261930b92d183dc3fd9193d90298bc52b
vlad <vladkalyuzhnyu@gmail.com>
2020-06-14 17:50:43 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
+++ app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
@@ -11,8 +11,6 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.renderer.XAxisRenderer
 import com.github.mikephil.charting.utils.Transformer
 import com.github.mikephil.charting.utils.ViewPortHandler
-import timber.log.Timber
-import kotlin.math.abs
 
 fun BarLineChartBase<*>.setSpecificLabels() {
     setXAxisRenderer(
@@ -27,32 +25,11 @@ private class SpecificLabelsXAxisRenderer(
 ) : XAxisRenderer(viewPortHandler, xAxis, transformer) {
 
     override fun computeAxisValues(min: Float, max: Float) {
-        /*Timber.e("min ${min.toLong()} max ${max.toLong()} ${((max - min) / GraphFragment.DAY).toInt()}")
-        mAxis.apply {
-            mEntries = FloatArray(((max - min) / GraphFragment.DAY).toInt()) {
-                Timber.e("!$it ${(min + it * GraphFragment.DAY).toLong()}")
-                min + it * GraphFragment.DAY
-            }
-            mEntryCount = mEntries.size
-            setCenterAxisLabels(false)
-        }*/
         with(mAxis) {
-            val range = abs(max - min)
-            if (labelCount == 0 || range <= 0 || java.lang.Float.isInfinite(range)) {
-                mEntries = floatArrayOf()
-                mCenteredEntries = floatArrayOf()
-                mEntryCount = 0
-                return
-            }
-            if (false) {
-                super.computeAxisValues(min, max)
-            }
-            mEntries = FloatArray((range / GraphFragment.DAY).toInt()) {
-                Timber.e("!$it ${(min + it * GraphFragment.DAY).toLong()}")
-                min + it * GraphFragment.DAY
+            mEntries = FloatArray((mAxisRange / GraphFragment.DAY).toInt()) {
+                mAxisMinimum + (it + 0.5f) * GraphFragment.DAY
             }
             mEntryCount = mEntries.size
-            setCenterAxisLabels(false)
             computeSize()
         }
     }
@@ -75,7 +52,7 @@ class BackBarChart @JvmOverloads constructor(
         if (drawBackground) {
             val x = toPixels(xChartMin)
             val width = toPixels(xChartMin + GraphFragment.DAY) - x
-            (0 until (xRange / GraphFragment.DAY).toInt() + 1).forEach {
+            (0 until (xRange / GraphFragment.DAY).toInt()).forEach {
                 if (it % 2 != 0) {
                     canvas.drawRect(
                         x + it * width,
