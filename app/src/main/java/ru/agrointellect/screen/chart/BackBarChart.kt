7fc1ae359b42bd615d9fcbb792d5834177fe530d
vlad <vladkalyuzhnyu@gmail.com>
2020-06-14 07:34:54 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
+++ app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
@@ -6,7 +6,6 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import com.github.mikephil.charting.charts.BarChart
-import kotlin.math.roundToInt
 
 class BackBarChart @JvmOverloads constructor(
     context: Context,
@@ -14,7 +13,7 @@ class BackBarChart @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : BarChart(context, attrs, defStyleAttr) {
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = Color.parseColor("#99F2F2F2")
     }
@@ -22,18 +21,19 @@ class BackBarChart @JvmOverloads constructor(
     var drawBackground = false
 
     override fun onDraw(canvas: Canvas) {
-        if (drawBackground) {
-            val min = mLeftAxisTransformer.getPixelForValues(xChartMin, yMin)
-            val max = mLeftAxisTransformer.getPixelForValues(xChartMin + GraphFragment.DAY, yMax)
-            val width = (max.x - min.x).toFloat()
-            (0 until ((xChartMax - xChartMin) / GraphFragment.DAY).roundToInt()).forEach {
+        val count = data?.getDataSetByIndex(0)?.entryCount ?: 0
+        if (drawBackground && count > 0) {
+            val x = mLeftAxisTransformer.getPixelForValues(xChartMin, 0f).x.toFloat()
+            val width = mLeftAxisTransformer.getPixelForValues(xChartMin + xRange / count, 0f)
+                .x.toFloat() - x
+            (0 until ((xChartMax - xChartMin) / GraphFragment.DAY).toInt() + 1).forEach {
                 if (it % 2 != 0) {
                     canvas.drawRect(
-                        min.x.toFloat() + it * width,
-                        min.y.toFloat(),
-                        min.x.toFloat() + width * (it + 1),
-                        max.y.toFloat(),
-                        paint
+                        x + it * width,
+                        contentRect.top,
+                        x + (it + 1) * width,
+                        contentRect.bottom,
+                        backgroundPaint
                     )
                 }
             }
