5105dd6279f244d2b5670ab676a43cb4826c7103
vlad <vladkalyuzhnyu@gmail.com>
2020-06-14 17:07:53 +0300

    changes

--- app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
+++ app/src/main/java/ru/agrointellect/screen/chart/BackBarChart.kt
@@ -12,46 +12,49 @@ import com.github.mikephil.charting.renderer.XAxisRenderer
 import com.github.mikephil.charting.utils.Transformer
 import com.github.mikephil.charting.utils.ViewPortHandler
 import timber.log.Timber
+import kotlin.math.abs
 
-fun BarLineChartBase<*>.setSpecificLabels(adjustCountToWidth: Boolean) {
+fun BarLineChartBase<*>.setSpecificLabels() {
     setXAxisRenderer(
-        SpecificLabelsXAxisRenderer(
-            viewPortHandler,
-            xAxis,
-            getTransformer(axisLeft.axisDependency),
-            adjustCountToWidth
-        )
+        SpecificLabelsXAxisRenderer(viewPortHandler, xAxis, getTransformer(axisLeft.axisDependency))
     )
 }
 
 private class SpecificLabelsXAxisRenderer(
     viewPortHandler: ViewPortHandler,
     xAxis: XAxis,
-    transformer: Transformer,
-    private val adjustCountToWidth: Boolean
+    transformer: Transformer
 ) : XAxisRenderer(viewPortHandler, xAxis, transformer) {
 
     override fun computeAxisValues(min: Float, max: Float) {
-        Timber.e("min $min max $max")
+        /*Timber.e("min ${min.toLong()} max ${max.toLong()} ${((max - min) / GraphFragment.DAY).toInt()}")
         mAxis.apply {
             mEntries = FloatArray(((max - min) / GraphFragment.DAY).toInt()) {
+                Timber.e("!$it ${(min + it * GraphFragment.DAY).toLong()}")
                 min + it * GraphFragment.DAY
             }
             mEntryCount = mEntries.size
             setCenterAxisLabels(false)
-        }
-
-        computeSize()
-
-        /*if (adjustCountToWidth) {
-            val width = mXAxis.mLabelRotatedWidth
-
-            while (width * mAxis.mEntryCount > mViewPortHandler.chartWidth / 2f) {
-                mAxis.mEntries =
-                    mAxis.mEntries.filterIndexed { index, _ -> index % 2 == 0 }.toFloatArray()
-                mAxis.mEntryCount = mAxis.mEntries.size
-            }
         }*/
+        with(mAxis) {
+            val range = abs(max - min)
+            if (labelCount == 0 || range <= 0 || java.lang.Float.isInfinite(range)) {
+                mEntries = floatArrayOf()
+                mCenteredEntries = floatArrayOf()
+                mEntryCount = 0
+                return
+            }
+            if (false) {
+                super.computeAxisValues(min, max)
+            }
+            mEntries = FloatArray((range / GraphFragment.DAY).toInt()) {
+                Timber.e("!$it ${(min + it * GraphFragment.DAY).toLong()}")
+                min + it * GraphFragment.DAY
+            }
+            mEntryCount = mEntries.size
+            setCenterAxisLabels(false)
+            computeSize()
+        }
     }
 }
 
