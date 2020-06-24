894c666d7c6b16487e96ecd90da84847f4f9853c
vlad <vladkalyuzhnyu@gmail.com>
2020-06-24 07:45:22 +0300

    changes

--- app/src/main/java/ru/agrointellect/MainApp.kt
+++ app/src/main/java/ru/agrointellect/MainApp.kt
@@ -35,15 +35,15 @@ class MainApp : Application(), KodeinAware {
         System.setProperty(
             "org.apache.poi.javax.xml.stream.XMLInputFactory",
             "com.fasterxml.aalto.stax.InputFactoryImpl"
-        );
+        )
         System.setProperty(
             "org.apache.poi.javax.xml.stream.XMLOutputFactory",
             "com.fasterxml.aalto.stax.OutputFactoryImpl"
-        );
+        )
         System.setProperty(
             "org.apache.poi.javax.xml.stream.XMLEventFactory",
             "com.fasterxml.aalto.stax.EventFactoryImpl"
-        );
+        )
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
