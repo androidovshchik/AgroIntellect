c2e10aac226e4e4c4da168b300ca342796947084
vlad <vladkalyuzhnyu@gmail.com>
2020-08-15 10:30:43 +0300

    changes

--- app/src/main/java/ru/agrointellect/remote/dto/RptsHerdAlignmentHistory.kt
+++ app/src/main/java/ru/agrointellect/remote/dto/RptsHerdAlignmentHistory.kt
@@ -7,19 +7,16 @@ import com.google.gson.annotations.SerializedName
 class RptsHerdAlignmentHistory : Table {
 
     override val columns: List<Column>
-        get() {
-            val lastDate = items.lastOrNull()?.date
-            val items = items.filter { it.date == lastDate }
-            return listOf(
-                Column("Кол-во животных", items.map { Row(it.group, it.countOfAnimalsInGroup) }),
-                Column("День доения", items.map { Row(it.group, it.averageDaysInMilk) }),
-                Column("Надой", items.map { Row(it.group, it.averageMilk) }),
-                Column("Ожидаемый надой", items.map { Row(it.group, it.expectedAverageMilk) })
+        get() = items.map {
+            Column(
+                "Группа ${it.groupNumber}", listOf(
+                    Row("Кол-во животных", it.countOfAnimalsInGroup),
+                    Row("День доения", it.averageDaysInMilk),
+                    Row("Надой", it.averageMilk),
+                    Row("Ожидаемый надой", it.expectedAverageMilk)
+                )
             )
         }
-
-    private val RptHerdAlignmentHistory.group
-        get() = "Группа $groupNumber"
 
     @SerializedName("rpt_herd_alignment_history")
     @Expose
