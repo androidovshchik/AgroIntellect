2cb9ffdb6c2be1778da25095a0eadcbe448076e2
vlad <vladkalyuzhnyu@gmail.com>
2020-10-26 09:05:30 +0300

    RptsPeriodicalFarmSummaryHistory

--- app/src/main/java/ru/agrointellect/remote/bean/RptPeriodicalFarmSummaryHistory.kt
+++ app/src/main/java/ru/agrointellect/remote/bean/RptPeriodicalFarmSummaryHistory.kt
@@ -7,7 +7,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 @Keep
-open class PeriodicalDataTable {
+open class RptPeriodicalDataTable {
 
     @SerializedName("date")
     @Expose
